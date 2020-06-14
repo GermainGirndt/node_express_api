924b303f9bad6a88ef6bff7b15c08421dc0563b4
GermainPereira <pereira.germain@outlook.com>
2020-06-14 16:58:31 -0300

    Added cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const mongoose = require("mongoose");
 // npm install require-dir for requiring automatically
 const requireDir = require('require-dir')
@@ -7,11 +8,14 @@ const requireDir = require('require-dir')
 // Initializing the API
 const app = express();
 
-// Creates a db schema
+// Creates a db schema for the Products
 require('./src/models/Products');
 
 // Tell the API to allow receiving json data
 app.use(express.json());
+
+// Tell the app for use cors (warning: this way, it's been granted access for all the domains)
+app.use(cors());
 
 // Initializing the DB
 mongoose.connect(
@@ -29,4 +33,8 @@ app.use('/api', require("./src/routes"));
 
 
 app.listen(3001);
+
+
+
+
 
